3da871afb1f43d9900a6002734581a04a0fef329
Josh Nederveld <joshnederveld@gmail.com>
2016-07-01 15:41:25 -0400

    Code formatting

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
-<div <?php CNP\Utility::echo_classes(['sidebar', 'column'], 'cnp_sidebar_div_classes'); ?>>
-	<div <?php CNP\Utility::echo_classes('sidebar__inside', 'cnp_sidebar_inside_div_classes'); ?>>
+<div <?php CNP\Utility::echo_classes( [ 'sidebar', 'column' ], 'cnp_sidebar_div_classes' ); ?>>
+	<div <?php CNP\Utility::echo_classes( 'sidebar__inside', 'cnp_sidebar_inside_div_classes' ); ?>>
 		<?php
 		do_action( 'cnp_sidebar_widgets' );
 		do_action( CNP\get_action( 'cnp_sidebar_widgets' ) );
