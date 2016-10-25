d72b19e6596962da090e7eee5d21ebe858e8edf2
Josh Nederveld <joshnederveld@gmail.com>
2016-10-25 16:31:09 -0400

    Update method names

--- content/content-archive.php
+++ content/content-archive.php
@@ -1,4 +1,4 @@
 <?php
 $archive_header = new CNP\PostHeaderArchive();
-$archive_header->getMarkup();
+$archive_header->get_markup();
 echo $archive_header->markup;
--- partials/header-section.php
+++ partials/header-section.php
@@ -1,6 +1,6 @@
 <?php
 $section_header = new CNP\SectionHeader();
-$section_header->getMarkup();
+$section_header->get_markup();
 echo $section_header->markup;
 
 get_template_part( 'partials/nav', 'breadcrumbs' );
